fd228cfc5763334a6a4fc84c1deef5189bb47f3a
Konredus <konradpeschka@hotmail.com>
2021-03-30 22:35:24 -0300

    Add files via upload

---
+++ examples/test_ds1307/test_ds1307.ino
@@ -0,0 +1,59 @@
+// Ejemplo de prueba para el modulo RTC_DS1307
+// Escrito ORIGINAL por JeeLabs, public domain
+// Traducido por Konrad Peschka @konredus
+
+#include <RTClib.h>
+
+RTC_DS1307 modulo_rtc;
+//este vector que contiene los nombres de la semana
+char DiasDeLaSemana[7][12] = {"Domingo", "Lunes", "Martes", "Miercoles", "Jueves", "Viernes", "Sabado"};
+
+void setup () {
+  Serial.begin(9600);
+
+  //este if chequea si el modulo_rtc inicio de forma correcta
+  if (! modulo_rtc.begin()) {
+    Serial.println("No pudimos encontrar ningun RTC");
+    Serial.flush();
+    abort();
+  }
+
+  // La primera vez o cuando hayamos cambiado la bateria del RTC
+  //seguramente necesitemos configurarlo a la fecha y el horario actual
+  //Para esas ocasiones tendremos que descomentar las 2 lineas de codigo
+  //que estan aqui abajo y colocar la fecha + la hora que queremos que tenga
+
+  //modulo_rtc.adjust(DateTime(F(__DATE__), F(__TIME__)));
+  //modulo_rtc.adjust(DateTime(2021, 3, 30, 22, 14, 0));
+
+  //luego cuando hayamos cargado este codigo, los volvemos a comentar
+  //y recargamos el codigo a nuestro Arduino para que no lo vuelva a pisar
+  //cada vez qeu se reinicia la placa Arduino.
+}
+
+void loop () {
+  //con "now" lo qeu hace esuna especie de SUB-variable
+  //donde guarda toda la info que llega del modulo
+  DateTime now = modulo_rtc.now();
+
+  //una vez que leimos el sensor, ya todo queda guardado
+  //en la variable "now", para leer los SUB-campos de "now"
+  //usamos el "punto" y colocamos "año", "mes", "dia","hora"
+  //"minuto", "segundo", todo en ingles.
+  Serial.print(now.year());
+  Serial.print('/');
+  Serial.print(now.month());
+  Serial.print('/');
+  Serial.print(now.day());
+  Serial.print(" (");
+  Serial.print(DiasDeLaSemana[now.dayOfTheWeek()]);
+  Serial.print(") ");
+  Serial.print(now.hour());
+  Serial.print(':');
+  Serial.print(now.minute());
+  Serial.print(':');
+  Serial.print(now.second());
+  Serial.println();
+
+  delay(1000);
+}
